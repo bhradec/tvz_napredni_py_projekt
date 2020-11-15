2dbfbd81bdb6d7704828c43a86aa33c5cf05a261
Bruno Hradec <bhradec@gmail.com>
2020-11-15 10:37:38 +0100

    prepravak u funkcije. Provjera maina.

--- display_pixels.py
+++ display_pixels.py
@@ -3,7 +3,16 @@ import numpy as np
 
 from PIL import Image
 
-input_img_path = sys.argv[1]
-input_img = Image.open(input_img_path)
-pixels = np.array(input_img)
-print(pixels)
+def display_pixels(img_path):
+    input_img = Image.open(img_path)
+    pixels = np.array(input_img)
+    print(pixels)
+
+if __name__ == "__main__":
+    img_path = sys.argv[1]
+
+    if img_path:
+        display_pixels(img_path)
+    else:
+        print("No image path given")
+        print("Help: display_pixels.py img_path")
---
+++ find_message.py
@@ -0,0 +1,16 @@
+import sys
+import numpy as np
+
+from PIL import Image
+
+def find_message(img_path):
+    print("TO DO")
+
+if __name__ == "__main__":
+    img_path = sys.argv[1]
+
+    if img_path:
+        hide_message(message, img_path)
+    else:
+        print("No image path given")
+        print("Help: message_find.py img_path")
